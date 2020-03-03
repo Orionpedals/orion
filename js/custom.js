59ef1eceeae893c03b46b62b01de3f0ec6e7056c
Billy Lundh <billy.lundh@gmail.com>
2020-03-03 17:41:57 +0100

    test

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(window).load(function(){
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    });
+
+    $(document).on('click','img',function(){
+
+      console.log( $('card card-1 boost').height() );
+      console.log("Click event works!");
+
+    });
+
+    
+});
+
+
+
